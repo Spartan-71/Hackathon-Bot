3f2aa741f3dd35e728eadec95ad492ec1132b1ab
Spartan-71 <anishdabhane71@gmail.com>
2026-01-11 00:07:10 +0530

    chore: remove explicit database URL

--- backend/db.py
+++ backend/db.py
@@ -3,9 +3,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = f"postgresql://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@db:5432/{os.getenv("POSTGRES_DB")}"
+print(f"----------{DATABASE_URL}")
+
+
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable is not set")
+
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
@@ -17,5 +21,6 @@ engine = create_engine(
         "options": "-c timezone=utc -c statement_timeout=60000"
     }
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
